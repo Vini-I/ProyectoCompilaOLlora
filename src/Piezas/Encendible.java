0fa35bcd6afa55c0143f242ea42b5700140edf35
Joches29 <jochaveshe@est.utn.ac.cr>
2025-07-25 17:37:17 -0600

    Se crea la interfaz Encendible
    Se agrega la interfaz Encendible, teniendo dos metodos genericos que permiten encender o apagar algo, al ser implementado en una clase.

---
+++ src/Piezas/Encendible.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Piezas;
+
+/**
+ *
+ * @author rodol
+ */
+public interface Encendible {
+    
+    public void encender();
+    public void apagar();
+    
+}
