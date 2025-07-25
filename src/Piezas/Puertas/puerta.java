0544246f872e845129e0da3cf8d8c614009bce2c
Lero <lesanchezro@est.utn.ac.cr>
2025-07-25 17:44:44 -0600

    Creacion de la clase puerta en el paquete Piezas.Puertas
    Se creo la clase puerta con sus atributos, metodos getters ,setters, constructor y sus funciones

---
+++ src/Piezas/Puertas/puerta.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Piezas.Puertas;
+
+/**
+ *
+ * @author llean
+ */
+public class puerta {
+    private boolean abierta;
+    private boolean bloqueada;
+
+    public boolean isAbierta() {
+        return abierta;
+    }
+
+    public boolean isBloqueada() {
+        return bloqueada;
+    }
+
+    public void setAbierta(boolean abierta) {
+        this.abierta = abierta;
+    }
+
+    public void setBloqueada(boolean bloqueada) {
+        this.bloqueada = bloqueada;
+    }
+
+    public puerta() {
+        this.abierta = false;
+        this.bloqueada = false;
+    }
+    
+    public void abrir() {
+        this.abierta = true;
+    }
+    
+    public void cerrar() {
+        this.abierta = false;
+    }
+    
+    public void bloquear() {
+        this.bloqueada = true;
+    }
+    
+    public void desbloquear() {
+        this.bloqueada = false;
+    }
+}
