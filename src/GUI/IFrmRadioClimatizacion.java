ac509bdeceb4887e330247f127218accca936c5a
Joches29 <jochaveshe@est.utn.ac.cr>
2025-07-30 14:59:39 -0600

    Modificacion en el Internal JFrame IFrmRadioClimatizacion
    Se modifica un poco el modo en que se muestra que el bluetooth esta activado o desactivado (Una imagen en vez de un radio button)

--- src/GUI/IFrmRadioClimatizacion.java
+++ src/GUI/IFrmRadioClimatizacion.java
@@ -35,10 +35,10 @@ public class IFrmRadioClimatizacion extends javax.swing.JInternalFrame {
         btnAMOn.setEnabled(radio.isEncendida());
         btnFMOn.setEnabled(radio.isEncendida());
         txtRadio.setText(radio.getNombreEstaciones());
-        rbtnBluetoothEstado.setVisible(radio.isBluetoothActivo());
         jslCiclar.setValue(radio.getIndiceEstaciones());
         jslCiclar.setEnabled(false);
         jslCiclar.setValueIsAdjusting(false);
+        
         
         jspVelocidad.setValue(climatizacion.getIndiceVelocidad());
         txtVelocidad.setText(climatizacion.getNombreVelocidades());  
@@ -78,23 +78,18 @@ public class IFrmRadioClimatizacion extends javax.swing.JInternalFrame {
         lblIconoTemperatura = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
         lblCircles = new javax.swing.JLabel();
-        btnBluetooth = new javax.swing.JToggleButton();
         btnEstadoRadio = new javax.swing.JButton();
         lblTemperatura = new javax.swing.JLabel();
-        rbtnBluetoothEstado = new javax.swing.JRadioButton();
+        btnBluetooth = new javax.swing.JToggleButton();
+        lblEstadoBluetooth = new javax.swing.JLabel();
 
         setMinimumSize(new java.awt.Dimension(480, 360));
         setPreferredSize(new java.awt.Dimension(432, 360));
-        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         lblTipoEstacion.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Iconos/off.png"))); // NOI18N
         lblTipoEstacion.setPreferredSize(new java.awt.Dimension(38, 39));
-        getContentPane().add(lblTipoEstacion, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 20, -1, -1));
-        getContentPane().add(jSeparator1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 62, 100, 10));
 
         jSeparator2.setOrientation(javax.swing.SwingConstants.VERTICAL);
-        getContentPane().add(jSeparator2, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 0, 10, 130));
-        getContentPane().add(jSeparator3, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 129, 420, 10));
 
         jslCiclar.setMajorTickSpacing(1);
         jslCiclar.setMaximum(4);
@@ -110,13 +105,11 @@ public class IFrmRadioClimatizacion extends javax.swing.JInternalFrame {
                 jslCiclarStateChanged(evt);
             }
         });
-        getContentPane().add(jslCiclar, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 40, -1, -1));
 
         txtVelocidad.setEditable(false);
         txtVelocidad.setFont(new java.awt.Font("Arial", 0, 18)); // NOI18N
         txtVelocidad.setForeground(new java.awt.Color(0, 0, 0));
         txtVelocidad.setHorizontalAlignment(javax.swing.JTextField.CENTER);
-        getContentPane().add(txtVelocidad, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 260, 270, -1));
 
         btnAMOn.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Iconos/am(1).png"))); // NOI18N
         btnAMOn.addActionListener(new java.awt.event.ActionListener() {
@@ -124,7 +117,6 @@ public class IFrmRadioClimatizacion extends javax.swing.JInternalFrame {
                 btnAMOnActionPerformed(evt);
             }
         });
-        getContentPane().add(btnAMOn, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 90, -1, -1));
 
         btnFMOn.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Iconos/fm(1).png"))); // NOI18N
         btnFMOn.addActionListener(new java.awt.event.ActionListener() {
@@ -132,7 +124,6 @@ public class IFrmRadioClimatizacion extends javax.swing.JInternalFrame {
                 btnFMOnActionPerformed(evt);
             }
         });
-        getContentPane().add(btnFMOn, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 90, -1, -1));
 
         jspVelocidad.setModel(new javax.swing.SpinnerNumberModel(0, 0, 5, 1));
         jspVelocidad.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
@@ -141,7 +132,6 @@ public class IFrmRadioClimatizacion extends javax.swing.JInternalFrame {
                 jspVelocidadStateChanged(evt);
             }
         });
-        getContentPane().add(jspVelocidad, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 190, -1, -1));
 
         jspTemperatura.setModel(new javax.swing.SpinnerNumberModel(24, 18, 35, 1));
         jspTemperatura.addChangeListener(new javax.swing.event.ChangeListener() {
@@ -149,40 +139,25 @@ public class IFrmRadioClimatizacion extends javax.swing.JInternalFrame {
                 jspTemperaturaStateChanged(evt);
             }
         });
-        getContentPane().add(jspTemperatura, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 190, -1, -1));
 
         lblTemperaturaTitulo.setFont(new java.awt.Font("Arial", 0, 18)); // NOI18N
         lblTemperaturaTitulo.setForeground(new java.awt.Color(0, 0, 0));
         lblTemperaturaTitulo.setText("Temperatura");
-        getContentPane().add(lblTemperaturaTitulo, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 150, -1, -1));
 
         jLabel2.setFont(new java.awt.Font("Arial", 0, 18)); // NOI18N
         jLabel2.setForeground(new java.awt.Color(0, 0, 0));
         jLabel2.setText("Velocidad");
-        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 150, -1, -1));
 
         txtRadio.setEditable(false);
         txtRadio.setFont(new java.awt.Font("Arial", 0, 18)); // NOI18N
         txtRadio.setForeground(new java.awt.Color(0, 0, 0));
         txtRadio.setHorizontalAlignment(javax.swing.JTextField.CENTER);
-        getContentPane().add(txtRadio, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 10, 270, -1));
 
         lblIconoTemperatura.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Iconos/thermometer.png"))); // NOI18N
-        getContentPane().add(lblIconoTemperatura, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 180, -1, -1));
 
         jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Iconos/speedometer.png"))); // NOI18N
-        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 180, -1, -1));
 
         lblCircles.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Iconos/circleWhite.png"))); // NOI18N
-        getContentPane().add(lblCircles, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 220, -1, -1));
-
-        btnBluetooth.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Iconos/bluetooth.png"))); // NOI18N
-        btnBluetooth.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnBluetoothActionPerformed(evt);
-            }
-        });
-        getContentPane().add(btnBluetooth, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 80, -1, -1));
 
         btnEstadoRadio.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Iconos/botonEncendido.png"))); // NOI18N
         btnEstadoRadio.addActionListener(new java.awt.event.ActionListener() {
@@ -190,14 +165,125 @@ public class IFrmRadioClimatizacion extends javax.swing.JInternalFrame {
                 btnEstadoRadioActionPerformed(evt);
             }
         });
-        getContentPane().add(btnEstadoRadio, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 90, -1, -1));
 
         lblTemperatura.setFont(new java.awt.Font("Arial", 0, 18)); // NOI18N
-        getContentPane().add(lblTemperatura, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 290, -1, -1));
 
-        rbtnBluetoothEstado.setSelected(true);
-        rbtnBluetoothEstado.setOpaque(true);
-        getContentPane().add(rbtnBluetoothEstado, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 90, -1, -1));
+        btnBluetooth.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Iconos/bluetooth.png"))); // NOI18N
+        btnBluetooth.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnBluetoothActionPerformed(evt);
+            }
+        });
+
+        lblEstadoBluetooth.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Iconos/switch-off.png"))); // NOI18N
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(30, 30, 30)
+                        .addComponent(lblTipoEstacion, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 108, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(12, 12, 12)
+                .addComponent(txtRadio, javax.swing.GroupLayout.PREFERRED_SIZE, 270, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(150, 150, 150)
+                .addComponent(jslCiclar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addComponent(jSeparator3, javax.swing.GroupLayout.PREFERRED_SIZE, 420, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(60, 60, 60)
+                .addComponent(jLabel2)
+                .addGap(111, 111, 111)
+                .addComponent(lblTemperaturaTitulo))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(40, 40, 40)
+                .addComponent(jLabel4)
+                .addGap(8, 8, 8)
+                .addComponent(jspVelocidad, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(66, 66, 66)
+                .addComponent(lblIconoTemperatura)
+                .addGap(8, 8, 8)
+                .addComponent(jspTemperatura, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(30, 30, 30)
+                .addComponent(lblCircles)
+                .addGap(26, 26, 26)
+                .addComponent(txtVelocidad, javax.swing.GroupLayout.PREFERRED_SIZE, 270, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(btnBluetooth, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(lblEstadoBluetooth)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(130, 130, 130)
+                        .addComponent(btnFMOn))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(60, 60, 60)
+                        .addComponent(btnAMOn))
+                    .addComponent(jSeparator2, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(200, 200, 200)
+                        .addComponent(btnEstadoRadio))))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(10, 10, 10)
+                        .addComponent(txtRadio, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(90, 90, 90)
+                        .addComponent(btnFMOn))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(90, 90, 90)
+                        .addComponent(btnAMOn))
+                    .addComponent(jSeparator2, javax.swing.GroupLayout.PREFERRED_SIZE, 130, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(90, 90, 90)
+                        .addComponent(btnEstadoRadio))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(40, 40, 40)
+                        .addComponent(jslCiclar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(20, 20, 20)
+                        .addComponent(lblTipoEstacion, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(3, 3, 3)
+                        .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(lblEstadoBluetooth)
+                        .addGap(13, 13, 13)
+                        .addComponent(jSeparator3, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(80, 80, 80)
+                        .addComponent(btnBluetooth)))
+                .addGap(1, 1, 1)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel2)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(10, 10, 10)
+                        .addComponent(lblTemperaturaTitulo)))
+                .addGap(8, 8, 8)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel4)
+                    .addComponent(lblIconoTemperatura)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(10, 10, 10)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jspVelocidad, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jspTemperatura, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addGap(8, 8, 8)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(lblCircles)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(40, 40, 40)
+                        .addComponent(txtVelocidad, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
+        );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -223,6 +309,8 @@ public class IFrmRadioClimatizacion extends javax.swing.JInternalFrame {
     private void btnBluetoothActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnBluetoothActionPerformed
         ImageIcon iconoBluetoothOn = new ImageIcon(getClass().getResource("/Iconos/bluetooth.png"));
         ImageIcon iconoBluetoothOff = new ImageIcon(getClass().getResource("/Iconos/deactivate.png"));
+        ImageIcon iconoOfff = new ImageIcon(getClass().getResource("/Iconos/switch-off.png"));
+        ImageIcon iconoOn = new ImageIcon(getClass().getResource("/Iconos/switch-on.png"));
         ImageIcon iconoOff = new ImageIcon(getClass().getResource("/Iconos/off.png"));
         if (!radio.isBluetoothActivo()){
             radio.activarBluetooth();
@@ -232,17 +320,18 @@ public class IFrmRadioClimatizacion extends javax.swing.JInternalFrame {
             radio.cambiar(jslCiclar.getValue());
             txtRadio.setText(radio.getNombreEstaciones());
             jslCiclar.setEnabled(false);
-            rbtnBluetoothEstado.setVisible(radio.isBluetoothActivo());
+            lblEstadoBluetooth.setIcon(iconoOn);
         }else{
             radio.desactivarBluetooth();
             btnBluetooth.setIcon(iconoBluetoothOn);
-            rbtnBluetoothEstado.setVisible(radio.isBluetoothActivo());
+            lblEstadoBluetooth.setIcon(iconoOfff);
         }
     }//GEN-LAST:event_btnBluetoothActionPerformed
 
     private void btnEstadoRadioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEstadoRadioActionPerformed
         ImageIcon iconoOff = new ImageIcon(getClass().getResource("/Iconos/off.png"));
         ImageIcon iconoBluetoothOn = new ImageIcon(getClass().getResource("/Iconos/bluetooth.png"));
+        ImageIcon iconoOfff = new ImageIcon(getClass().getResource("/Iconos/switch-off.png"));
         if(!radio.isEncendida()){
             radio.encender();
             btnBluetooth.setEnabled(radio.isEncendida());
@@ -255,7 +344,7 @@ public class IFrmRadioClimatizacion extends javax.swing.JInternalFrame {
             btnBluetooth.setIcon(iconoBluetoothOn);
             btnBluetooth.setSelected(false);
             btnBluetooth.setEnabled(radio.isEncendida());
-            rbtnBluetoothEstado.setVisible(radio.isBluetoothActivo());
+            lblEstadoBluetooth.setIcon(iconoOfff);
             btnAMOn.setEnabled(radio.isEncendida());
             btnFMOn.setEnabled(radio.isEncendida());
             txtRadio.setText(radio.getNombreEstaciones());
@@ -343,11 +432,11 @@ public class IFrmRadioClimatizacion extends javax.swing.JInternalFrame {
     private javax.swing.JSpinner jspTemperatura;
     private javax.swing.JSpinner jspVelocidad;
     private javax.swing.JLabel lblCircles;
+    private javax.swing.JLabel lblEstadoBluetooth;
     private javax.swing.JLabel lblIconoTemperatura;
     private javax.swing.JLabel lblTemperatura;
     private javax.swing.JLabel lblTemperaturaTitulo;
     private javax.swing.JLabel lblTipoEstacion;
-    private javax.swing.JRadioButton rbtnBluetoothEstado;
     private javax.swing.JTextField txtRadio;
     private javax.swing.JTextField txtVelocidad;
     // End of variables declaration//GEN-END:variables
