9e764f6d5597ae3009deef62fc5e5e13353512c9
Marco Vinicio Vasquez Barrantes <mavasquezba@est.utn.ac.cr>
2025-07-30 14:54:27 -0600

    Creacion de la clase Puerta
    Se vuelve a crear la clase puerta exactamente igual que en su estado anterior pero con el nombre correcto para corregir el problema que se estaba presentando con la misma

---
+++ src/Piezas/Puertas/Puerta.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Piezas.Puertas;
+
+/**
+ *
+ * @author llean
+ */
+public class Puerta {
+    private boolean abierta;
+    private boolean bloqueada;
+
+    public boolean isAbierta() {
+        return abierta;
+    }
+
+    public boolean isBloqueada() {
+        return bloqueada;
+    }
+
+    public Puerta() {
+        this.abierta = false;
+        this.bloqueada = false;
+    }
+
+    public void abrir() {
+        this.abierta = true;
+    }
+
+    public void cerrar() {
+        this.abierta = false;
+    }
+
+    public void bloquear() {
+        this.bloqueada = true;
+    }
+
+    public void desbloquear() {
+        this.bloqueada = false;
+    }
+}
